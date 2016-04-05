f38ea54b2afbeb949173b6e09439b28f69361c02
didiepetrova <didiepetrova@yahoo.com>
2016-04-05 14:14:26 +0300

    update cordova, remove custom plugins, updated logic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,8 @@
-(function() {
-    var everlive = new Everlive("s2voxDcH8vu5ossW");
+(function () {
+    // To store the images on the Cloud, please specify valid APP ID. You could also refer to the online documentation:
+    // http://docs.telerik.com/platform/backend-services/javascript/files/files-tutorial-device-camera
+
+    var everlive = new Everlive("YOUR APP ID");
     document.addEventListener("deviceready", function () {
         window.listview = kendo.observable({
             addImage: function() {
@@ -8,10 +11,12 @@
                         Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
                         ContentType: "image/jpeg",
                         base64: data
-                    }).then(loadPhotos);
+                    }).then(loadPhotos,
+                            function (err) {
+                            alert("Could not upload image" + err.message);});
                 };
                 var error = function() {
-                    navigator.notification.alert("Unfortunately we could not add the image");
+                    navigator.notification.alert("Unfortunately the image was not captured");
                 };
                 var config = {
                     destinationType: Camera.DestinationType.DATA_URL,
@@ -34,9 +39,42 @@
                     dataSource: files,
                     template: "<img src='#: data #'>"
                 });
+            },
+            function (err) {
+                    alert("Could not fetch images: " + err.message);
+                
             });
         }
         loadPhotos();
         navigator.splashscreen.hide();
     });
+
+    //TEST LESSON 2, store images locally
+    //document.addEventListener("deviceready", function () {
+    //    window.listview = kendo.observable({
+    //        addImage: function () {
+    //            var success = function (data) {
+    //                $("#images")
+    //                    .data("kendoMobileListView")
+    //                    .prepend(["data:image/jpeg;base64," + data]);
+    //            };
+    //            var error = function () {
+    //                navigator.notification.alert("Unfortunately we could not add the image");
+    //            };
+    //            var config = {
+    //                destinationType: Camera.DestinationType.DATA_URL,
+    //                targetHeight: 400,
+    //                targetWidth: 400
+    //            };
+    //            navigator.camera.getPicture(success, error, config);
+    //        }
+    //    });
+    //    var app = new kendo.mobile.Application(document.body, { skin: "nova" });
+
+    //    $("#images").kendoMobileListView({
+    //        dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+    //        template: "<img src='#: data #'>"
+    //    });
+    //});
+
 }());
